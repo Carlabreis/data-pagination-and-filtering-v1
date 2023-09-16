8a391e06ed606253b28252881912af25f1c45605
Carla Borges Reis <carlareis.dev.com>
2023-09-16 16:06:12 -0700

    organized code order

--- js/script.js
+++ js/script.js
@@ -3,54 +3,13 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-/*
-Reference element selectors
-*/
+// Reference element selectors
 const header = document.querySelector(".header");
 const studentList = document.querySelector(".student-list");
 const linkList = document.querySelector(".link-list");
 
-// define how many items to display per page
+// Define how many items to display per page
 const itemsPerPage = 9;
-
-/*
-Create and insert search bar
- */
-const searchBar = `
-   <label for="search" class="student-search">
-      <span>Search by name</span>
-      <input id="search" placeholder="Search by name...">
-      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-   </label>
-`;
-header.insertAdjacentHTML("beforeend", searchBar);
-const searchInput = document.querySelector("#search");
-
-/*
-Add event listener to search bar input to search dinamically
-*/
-searchInput.addEventListener("keyup", () => {
-  const newList = [];
-  const userInput = searchInput.value.toLowerCase();
-
-  for (i = 0; i < data.length; i++) {
-    let studentName = `${data[i].name.first} ${data[i].name.last}`;
-    studentName = studentName.toLowerCase();
-
-    if (studentName.includes(userInput)) {
-      newList.push(data[i]);
-    }
-  }
-
-  if (newList.length > 0) {
-    addPagination(newList);
-    showPage(newList, 1);
-  } else {
-    const html = "<h3>No results found.</h3>";
-    studentList.innerHTML = html;
-    linkList.innerHTML = "";
-  }
-});
 
 /*
    Create and insert the html to the ul with the class of student-list to show student list per page
@@ -110,6 +69,44 @@ function addPagination(list) {
     }
   });
 }
+
+
+// Create and insert search bar
+const searchBar = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>
+`;
+header.insertAdjacentHTML("beforeend", searchBar);
+const searchInput = document.querySelector("#search");
+
+
+// Add event listener to search bar input to search dinamically
+searchInput.addEventListener("keyup", () => {
+  const newList = [];
+  const userInput = searchInput.value.toLowerCase();
+
+  for (i = 0; i < data.length; i++) {
+    let studentName = `${data[i].name.first} ${data[i].name.last}`;
+    studentName = studentName.toLowerCase();
+
+    if (studentName.includes(userInput)) {
+      newList.push(data[i]);
+    }
+  }
+
+  if (newList.length > 0) {
+    addPagination(newList);
+    showPage(newList, 1);
+  } else {
+    const html = "<h3>No results found.</h3>";
+    studentList.innerHTML = html;
+    linkList.innerHTML = "";
+  }
+});
+
 
 // Call functions
 addPagination(data);
