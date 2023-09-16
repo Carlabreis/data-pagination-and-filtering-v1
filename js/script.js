e2e78b0d09d21a0847f1dd54454d91fc491d897d
Carla Borges Reis <carlareis.dev.com>
2023-09-16 15:47:23 -0700

    added search bar and functionality

--- js/script.js
+++ js/script.js
@@ -3,8 +3,58 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+/*
+Reference element selectors
+*/
+const header = document.querySelector(".header");
+const studentList = document.querySelector(".student-list");
+const linkList = document.querySelector(".link-list");
+
+
 // define how many items to display per page
 const itemsPerPage = 9;
+
+
+/*
+Create and insert search bar
+ */
+const searchBar = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>
+`;
+header.insertAdjacentHTML("beforeend", searchBar);
+const searchInput = document.querySelector("#search");
+
+
+/*
+Add event listener to search bar input to search dinamically
+*/
+searchInput.addEventListener("keyup", () => {
+   const newList = [];
+   const userInput = searchInput.value.toLowerCase();
+   
+   for (i = 0; i < data.length; i++){
+      let studentName = `${data[i].name.first} ${data[i].name.last}`;
+      studentName = studentName.toLowerCase();
+      
+      if (studentName.includes(userInput)) {
+         newList.push(data[i]);
+      }
+   }
+
+   if (newList.length > 0) {
+      addPagination(newList);
+      showPage(newList, 1);
+   } else {
+      const html = "<h3>No results found...</h3>";
+      studentList.innerHTML = html;
+      linkList.innerHTML = "";
+   }
+});
+
 
 /*
    Create and insert the html to the ul with the class of student-list to show student list per page
@@ -14,9 +64,8 @@ const itemsPerPage = 9;
 function showPage(list, page) {
   const startIndex = page * itemsPerPage - itemsPerPage;
   const endIndex = page * itemsPerPage;
-  const studentList = document.querySelector(".student-list");
   studentList.innerHTML = "";
-  for (let i = startIndex; i < endIndex; i++) {
+  for (let i = startIndex; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
       const html = `
             <li class="student-item cf">
@@ -41,7 +90,6 @@ function showPage(list, page) {
 */
 function addPagination(list) {
   const numberOfButtons = Math.ceil(list.length / itemsPerPage);
-  const linkList = document.querySelector(".link-list");
   linkList.innerHTML = "";
   for (let i = 1; i <= numberOfButtons; i++) {
     const html = `
